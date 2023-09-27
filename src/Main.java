1317354d98346627c0d53cc245089c934f4f04ca
Juan Salas <sagajc03@gmail.com>
2023-09-26 22:51:59 -0600

    Eliminar libros

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
+
     public static HashMap<String, Libro> libros = new HashMap<>();
     public static void menuA(){
+        /*
+        * Funcion de la logica del programa
+        * */
         Scanner sc = new Scanner(System.in);
+        String aux;
         boolean activo = true;
         int opc;
         String texto_opcion = "1 - Agregar libros\n" +
@@ -22,8 +27,10 @@ public class Main {
                     agregarLibros();
                     break;
                 case 2:
+                    eliminarLibros();
                     break;
                 case 3:
+                    buscarLibro();
                     break;
                 case 4:
                     break;
@@ -43,38 +50,66 @@ public class Main {
         }
     }
 
-    public static void agregarLibros(){
+    private static void buscarLibro() {
         Scanner sc = new Scanner(System.in);
-        String esono;
-        String autor;
-        String titulo;
-        int num_paginas = 0;
-        String categoria;
-        boolean secuela = false;
-
-        System.out.println("Titulo> ");
-        titulo = sc.nextLine();
-
-        System.out.println("Autor> ");
-        autor = sc.nextLine();
-
-        System.out.println("Numero de paginas> ");
-        num_paginas = Integer.parseInt(sc.nextLine());
-
-        System.out.println("Categoria> ");
-        categoria = sc.nextLine();
-
-        System.out.println("Es secuela (s/n)> ");
-        esono = sc.nextLine();
-        if (esono.equalsIgnoreCase("s")){
-            secuela = true;
+        System.out.println("Escribe el libro a buscar");
+        String aux = sc.nextLine();
+        if(libros.containsKey(aux)){
+            System.out.println(libros.get(aux).mostrarDetalles());
+        }else {
+            System.out.println("No se encontro el libro");
         }
-
-        Libro l = new Libro(autor, titulo, num_paginas, categoria, secuela);
-        libros.put(titulo, l);
     }
 
+    public static void agregarLibros(){
+        /*
+        * Agrega libros al HashMap
+        * */
+        try {
+            Scanner sc = new Scanner(System.in);
+            String esono;
+            String autor;
+            String titulo;
+            int num_paginas = 0;
+            String categoria;
+            boolean secuela = false;
+
+            System.out.println("Titulo> ");
+            titulo = sc.nextLine();
+
+            System.out.println("Autor> ");
+            autor = sc.nextLine();
+
+            System.out.println("Numero de paginas> ");
+            num_paginas = Integer.parseInt(sc.nextLine());
+
+            System.out.println("Categoria> ");
+            categoria = sc.nextLine();
+
+            System.out.println("Es secuela (s/n)> ");
+            esono = sc.nextLine();
+            if (esono.equalsIgnoreCase("s")) {
+                secuela = true;
+            }
+
+            Libro l = new Libro(autor, titulo, num_paginas, categoria, secuela);
+            libros.put(titulo, l);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void eliminarLibros(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe el libro a eliminar");
+        String aux = sc.nextLine();
+        libros.remove(aux);
+        System.out.println("Se elimino " + aux);
+    }
     public static void visualizarLibros(){
+        /*
+        * Pasa por todos los libros del HashMap
+        * */
         for (Libro libro : libros.values()) {
             System.out.println(libro.mostrarDetalles());
         }
